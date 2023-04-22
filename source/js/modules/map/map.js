96417319258e26e25bad355a0aa0de45b41da8c3
Dmitry Obryvkov <dmitry.obryvkov213@mail.ru>
2023-04-22 11:28:14 +0300

    Стилизует блок контактов для декстопа

---
+++ source/js/modules/map/map.js
@@ -0,0 +1,20 @@
+
+function init() {
+  let map = new ymaps.Map('map', {
+    center: [59.93814654961048, 30.32288629629509],
+    zoom: 16,
+  });
+
+  let placemark = new ymaps.Placemark([59.93760280814356, 30.322736092590247], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: '../img/sprite/marker.svg',
+    iconImageSize: [18, 22],
+    iconImageOffset: [-9, -10],
+  });
+
+  map.controls.remove('rulerControl');
+
+  map.geoObjects.add(placemark);
+}
+
+export const Map = init;
